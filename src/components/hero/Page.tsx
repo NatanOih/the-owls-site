140b32b965a71de9b42ee452dcfeebbbc4d4fe41
Natan Oihman <natanoih@gmail.com>
2024-09-14 13:05:33 +0300

    new layout2

--- src/components/hero/Page.tsx
+++ src/components/hero/Page.tsx
@@ -4,8 +4,18 @@ import UpcommingShows from "./UpcommingShows";
 
 export default function HomePage() {
   return (
-    <main className="  h-fit  gap-20 p-12 flex flex-col items-center justify-center  ">
+    <main className="  h-fit select-none  gap-20 p-12 flex flex-col items-center justify-center  ">
       <LogoBlock />
+      <h1 className="text-6xl font-extrabold text-white/90">
+        {" "}
+        The Owls - The Who Cover Band
+      </h1>
+      <p className="text-white/90 text-2xl w-[30vw]">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore,
+        delectus. Repellat impedit vero minima neque illo libero repellendus
+        deleniti cupiditate tenetur, rerum odit, aspernatur eveniet, obcaecati
+        consequuntur. Repudiandae, a sed.
+      </p>
       <div className="grid grid-cols-1 gap-28 items-center px-4">
         <ShowData />
         <UpcommingShows />
--- src/components/hero/ShowData.tsx
+++ src/components/hero/ShowData.tsx
@@ -2,7 +2,7 @@ export default function ShowData() {
   return (
     <div className="flex flex-col gap-20 md:scale-110 justify-start">
       <div className="bg-white p-8 shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] transform -rotate-2">
-        <h1 className="text-6xl font-black mb-4 text-red-600">THE OWLS</h1>
+        {/* <h1 className="text-6xl font-black mb-4 text-red-600">THE OWLS</h1> */}
         <p className="text-2xl font-bold italic">
           Rock `n` Roll might not solve your problems, but it does let you dance
           all over them
